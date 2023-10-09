c28ad3611d12b6ce54867c6a92f17f4443c42407
EtinRuyi <omoruyi.idowu@yahoo.com>
2023-10-09 20:34:31 +0100

    Contact-Book-API using ASP.NET Core Web API

---
+++ ContactBookAPI.Model/Entities/Shared/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactBookAPI.Model.Entities.Shared
+{
+    public class BaseEntity
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public DateTime DateUpdated { get; set; } = DateTime.Now;
+        public bool IsDreprecated { get; set; }
+    }
+}
